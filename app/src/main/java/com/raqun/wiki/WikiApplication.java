bc9a9bdf248ebd1c5a16230e7f5d358535322a17
tyln <taylan.sbrcn@gmail.com>
2017-04-11 22:10:24 +0300

    prod release v1.0.0

--- app/src/main/java/com/raqun/wiki/WikiApplication.java
+++ app/src/main/java/com/raqun/wiki/WikiApplication.java
@@ -3,10 +3,12 @@ package com.raqun.wiki;
 import android.app.Application;
 import android.support.annotation.NonNull;
 
+import com.crashlytics.android.Crashlytics;
 import com.raqun.wiki.data.api.ApiModule;
 import com.raqun.wiki.data.source.DaggerSearchRepositoryComponent;
 import com.raqun.wiki.data.source.SearchRepositoryComponent;
 import com.raqun.wiki.data.source.SearchRepositoryModule;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by tyln on 16.08.16.
@@ -18,6 +20,7 @@ public class WikiApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         mSearcRepositoryComponent = DaggerSearchRepositoryComponent.builder()
                 .applicationModule(new ApplicationModule((this)))
                 .searchRepositoryModule(new SearchRepositoryModule())
